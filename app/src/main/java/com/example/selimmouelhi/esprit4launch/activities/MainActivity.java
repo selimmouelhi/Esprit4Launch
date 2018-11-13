46d83b81be19f09fc037dfd860806ff65204eeb1
selimmouelhi <slmmlh30@gmail.Com>
2018-11-13 22:15:19 +0100

    login facebook

--- app/src/main/java/com/example/selimmouelhi/esprit4launch/activities/MainActivity.java
+++ app/src/main/java/com/example/selimmouelhi/esprit4launch/activities/MainActivity.java
@@ -1,16 +1,28 @@
 package com.example.selimmouelhi.esprit4launch.activities;
 
 import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Base64;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
 import com.example.selimmouelhi.esprit4launch.R;
+import com.facebook.AccessToken;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
 import com.google.android.gms.auth.api.Auth;
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
@@ -24,6 +36,17 @@ import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.common.api.ResultCallback;
 import com.google.android.gms.common.api.Status;
 import com.google.android.gms.tasks.Task;
+import com.squareup.picasso.Picasso;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.security.Signature;
+import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener,GoogleApiClient.OnConnectionFailedListener {
     private static final int RC_SIGN_IN =9001;
@@ -34,14 +57,20 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private TextView emailView;
     private TextView urlView;
     private Button log_out;
+    private CallbackManager callbackManager;
+    private LoginButton loginButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        FacebookSdk.sdkInitialize(getApplicationContext());
         setContentView(R.layout.activity_main);
         nameView = findViewById(R.id.name);
         emailView = findViewById(R.id.email);
         urlView = findViewById(R.id.url);
         log_out = findViewById(R.id.log_out);
+        loginButton = findViewById(R.id.login_button);
+
 
         GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
                 .requestEmail()
@@ -55,6 +84,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         signInButton.setSize(SignInButton.SIZE_STANDARD);
         signInButton.setOnClickListener(this);
         log_out.setOnClickListener(this);
+        loginButton.setOnClickListener(this);
+        printKeyHash();
 
 
 
@@ -64,6 +95,80 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     ;
 }
 
+private void signInFacebook(){
+
+    callbackManager = CallbackManager.Factory.create();
+    loginButton.setReadPermissions(Arrays.asList("public_profile","email","user_birthday"));
+    loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+        @Override
+        public void onSuccess(LoginResult loginResult) {
+            String accessToken = loginResult.getAccessToken().getToken();
+            GraphRequest request = GraphRequest.newMeRequest(loginResult.getAccessToken(), new GraphRequest.GraphJSONObjectCallback() {
+                @Override
+                public void onCompleted(JSONObject object, GraphResponse response) {
+                    Log.d("response ",response.toString());
+                    getData(object);
+
+                }
+            });
+            Bundle parametres = new Bundle();
+            parametres.putString("fields","id,email,birthday,friends");
+            request.setParameters(parametres);
+            request.executeAsync();
+
+        }
+
+
+
+
+
+        @Override
+        public void onCancel() {
+
+        }
+
+        @Override
+        public void onError(FacebookException error) {
+
+        }
+    });
+
+    if (AccessToken.getCurrentAccessToken()!= null){
+
+        nameView.setText(AccessToken.getCurrentAccessToken().getUserId());
+    }
+
+
+
+}
+
+    private void getData(JSONObject object) {
+        try {
+            /*URL profile_picture = new URL("htttps://graph.facebook.com/"+object.getString("id")+"/picture?width=250&height=250");
+            Picasso.with(this).load(profile_picture.toString())into();*/
+            emailView.setText(object.getString("email"));
+            nameView.setText(object.getString("first_name"));
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    private void printKeyHash() {
+        try {
+            PackageInfo info =getPackageManager().getPackageInfo("com.example.selimmouelhi.esprit4launch",PackageManager.GET_SIGNATURES);
+            for (android.content.pm.Signature signature :info.signatures){
+                MessageDigest md = MessageDigest.getInstance("SHA");
+                md.update(signature.toByteArray());
+                Log.d("KeyBash",Base64.encodeToString(md.digest(),Base64.DEFAULT));
+            }
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+    }
+
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
@@ -72,6 +177,11 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             // a listener.
             GoogleSignInResult result= Auth.GoogleSignInApi.getSignInResultFromIntent(data);
             handleSignInResult(result);
+
+        }
+        else
+        {
+            callbackManager.onActivityResult(requestCode,resultCode,data);
         }
     }
 
@@ -115,7 +225,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         log_out.setVisibility(View.VISIBLE);
     }
         else
-    {signInButton.setVisibility(View.VISIBLE);
+    {
+        signInButton.setVisibility(View.VISIBLE);
     }
 
     }
@@ -134,6 +245,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             case R.id.log_out:
                 signOut();
             break;
+            case R.id.login_button:
+                signInFacebook();
+                break;
         }
 
     }
